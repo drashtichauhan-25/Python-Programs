be23f4e218a6486022f9cecb22a78f32e4775421
drashtichauhan-25 <djchauhan2005@gmail.com>
2026-07-30 12:54:30 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,39 @@
+#Write a program to illustrate the use of tuples and sets with basic operations. 
+
+#tuple:
+
+
+tuple=(10,20,30,40,50)
+print(tuple)
+print(tuple[:4])
+print(tuple[-2])
+
+#tuple.append('60','70','80')
+#print('After append :',tuple)
+
+#tuple.remove(60)
+#print('After Remove :',tuple)
+
+#tuple.pop()
+#print('after pop:',tuple)
+
+#tuple.reverse()
+#print('after reverse:',tuple)
+
+
+#set :
+
+set={100,200,300,400,500}
+print(set)
+
+set.add(600)
+print(set)
+
+set.update([600,700,800])
+print(set)
+
+set.pop()
+print(set)
+
+#set.delete(600)
+#print(set)
