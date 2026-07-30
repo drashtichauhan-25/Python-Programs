26c9f4d08b4dc0808a3ba57424196615d63f20ad
drashtichauhan-25 <djchauhan2005@gmail.com>
2026-07-30 12:56:50 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,11 @@
+number =int(input('enter number:'))
+
+if number<0:
+    print('number is negative' )
+
+elif number==0:
+    print('number is zero')
+    
+else:
+    print('number is positive')
+    
