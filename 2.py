fe40dbe41b2ccdacd2f33bd0bc6cd84d15dd02b0
drashtichauhan-25 <djchauhan2005@gmail.com>
2026-07-30 12:40:19 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+value1= 50
+value2=6.9
+value4='drashti'
+value3=[1,2,3]
+value5=(1,2,3)
+value6={1,2,3}
+value7=True
+
+print(type(value1))
+print(type(value2))
+print(type(value4))
+print(type(value3))
+print(type(value5))
+print(type(value6))
+
