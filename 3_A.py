d325e5f128f7fb4f8c59f3354f6c89ffb8d9268d
drashtichauhan-25 <djchauhan2005@gmail.com>
2026-07-30 12:41:56 +0530

    Add files via upload

---
+++ 3_A.py
@@ -0,0 +1,10 @@
+# Write a program to perform arithmetic relational and logical operations using Python operators
+
+val1=float(input('Enter first number:'))
+val2=float(input('Enter second number:'))
+print("SUM:",val1+val2)
+print("SUB:",val1-val2)
+print("MULTI:",val1*val2)
+print("DIVISION:",val1/val2)
+
+
