be59754e21539277a0a15b2116163da3801a4863
drashtichauhan-25 <djchauhan2005@gmail.com>
2026-07-30 12:53:54 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,8 @@
+# Write a program to demonstrate string operations including slicing formatting and built-in string functions. 
+
+Name= 'Drashti'
+print(Name[0:1])
+print(Name[1:5])
+print(Name[0:1])
+print(Name[2:7])
+print(Name[-2:-3])
